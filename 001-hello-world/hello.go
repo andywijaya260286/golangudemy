8b76d944024be9479d55abe9ed5da095b8b9cff8
unknown <andy_wijaya260286@yahoo.com>
2019-04-29 12:48:28 +0700

    Adding new folder

--- 001-hello-world/hello.go
+++ 001-hello-world/hello.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Go!!")
-	fmt.Println("Let get started!!")
+	n, err := fmt.Println("Hello Go!!")
+	fmt.Println(n)
+	fmt.Println(err)
+
+	// Short declaration operator
+	// Declare a variable and Assign a value 
+	a := 42
+	fmt.Println("nilai a = ", a)
+	a = 45 + 1
+	fmt.Println("nilai a update = ", a)
+
+	n2, _ := fmt.Println("Hello Go 2nd!!")
+	fmt.Println(n2)
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("i = ", i)
+		}
+	}
+
 }
---
+++ 002-var-keyword/varkeyword.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+var y = 46
+var aa, bb bool
+var zz, yy = true, "yy"
+
+func main() {
+	x:=42
+	fmt.Println(x)
+	fmt.Println(y)
+
+	fmt.Println(aa)
+	fmt.Println("yy value = ",yy,", zz value = ",zz)
+}
