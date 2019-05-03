4eb84ba26f2553c080ccf5530cb4baef4a4b5305
unknown <andy_wijaya260286@yahoo.com>
2019-05-03 11:19:52 +0700

    add iota

--- 010-constant/constant.go
+++ 010-constant/constant.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
-func main() {
+const a = 42
+const(
+	b float64 = 88.889
+	c = "Testing"
+)
 
+func main() {
+	fmt.Println("a = ",a)
+	fmt.Println("b = ",b)
+	fmt.Println("c = ",c)
+
+	fmt.Printf("%T\n",a)
+	fmt.Printf("%T\n",b)
+	fmt.Printf("%T\n",c)
 }
